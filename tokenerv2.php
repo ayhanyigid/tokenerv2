1dc81386c9b510c748a655404afb8ed7b750f095
Ayhan Yigid <ayhan@yigid.com>
2021-12-16 14:30:57 +0300

    ReFormat

--- tokenerv2.php
+++ tokenerv2.php
@@ -11,28 +11,28 @@
  */
 class TokenerV2
 {
-	private $key     = 'Turkiye';
-	public  $text    = 'www.yigid.com';
-    public  $chipper = 'AES-128-CBC';
+    private $key = 'Turkiye';
+    public $text = 'www.yigid.com';
+    public $chipper = 'AES-128-CBC';
 
     /**
      * TokenerV2 constructor.
      * @throws Exception
      */
     public function __construct()
-	{
-		if (empty($this->key)) {
-			throw new Exception("The key is empty");						
-		}
-	}
+    {
+        if (empty($this->key)) {
+            throw new Exception("The key is empty");
+        }
+    }
 
     /**
      * Encrypts plaintext with given parameters
      * @return string
      * @throws Exception
      */
-    public function encrypt() : string
-	{
+    public function encrypt(): string
+    {
         if (empty($this->chipper)) {
             throw new Exception("The Chipper is empty");
         }
@@ -61,8 +61,8 @@ class TokenerV2
             $this->key,
             true
         );
-        return base64_encode( $iv.$hmac.$cipher_text_raw );
-	}
+        return base64_encode($iv . $hmac . $cipher_text_raw);
+    }
 
 
     /**
@@ -72,10 +72,10 @@ class TokenerV2
      * @return string|bool
      * @throws Exception
      */
-	public function decrypted(string $encrypted) :string|bool
-	{
-		if (empty($encrypted))
-			throw new Exception("The encrypted text is empty");
+    public function decrypted(string $encrypted): string|bool
+    {
+        if (empty($encrypted))
+            throw new Exception("The encrypted text is empty");
 
         if (empty($this->chipper))
             throw new Exception("The Chipper is empty");
@@ -87,15 +87,14 @@ class TokenerV2
         $decode_text = base64_decode($encrypted);
         $iv_len = openssl_cipher_iv_length($this->chipper);
         $iv = substr($decode_text, 0, $iv_len);
-        $hmac = substr($decode_text, $iv_len, $sha2len=32);
-        $cipher_text = substr($decode_text, $iv_len+$sha2len);
+        $hmac = substr($decode_text, $iv_len, $sha2len = 32);
+        $cipher_text = substr($decode_text, $iv_len + $sha2len);
         $calc_mac = hash_hmac('sha256', $cipher_text, $this->key, true);
-        if (hash_equals($hmac, $calc_mac))
-        {
+        if (hash_equals($hmac, $calc_mac)) {
             return openssl_decrypt($cipher_text, $this->chipper, $this->key, OPENSSL_RAW_DATA, $iv);
         }
         return false;
-	}
+    }
 
     /**
      * @return string
@@ -195,8 +194,8 @@ class TokenerV2
             'Düzce'
         );
         shuffle($list);
-        $len = count($list)-1;
-        $rand = rand(0,$len);
+        $len = count($list) - 1;
+        $rand = rand(0, $len);
         return $list[$rand];
     }
 }
